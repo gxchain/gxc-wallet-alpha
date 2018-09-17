9e4ae937f370a41dd43645f5b539af9bfb3b5d83
刘宗源 <940101379@qq.com>
2018-09-17 15:41:01 +0800

    first commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'gxc-wallet-alpha'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
